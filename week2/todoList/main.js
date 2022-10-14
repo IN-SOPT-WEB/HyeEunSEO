82fd86aa61c4902947708b88a50a6711d1583a52
Hennization <shy6295@ewhain.net>
2022-10-14 16:05:39 +0900

    [ feat ] 삭제 버튼 누르면 li 삭제

--- week2/todoList/main.js
+++ week2/todoList/main.js
@@ -8,6 +8,8 @@ const todos = $$("#todos > section");
 const inputs = $$(".todos__input");
 const addBtns = $$(".todos__add-btn");
 const allItems = $$(".todos__items");
+const deleteBtns = $$(".todos__delete-btn");
+const deleteBtn = $(".todos__delete-btn");
 
 // 오늘만 보기, 내일만 보기, 함께 보기 버튼에 따라 레이아웃 변경
 nav.addEventListener("click", (e) => {
@@ -42,22 +44,29 @@ inputs.forEach((input, index) => {
 // input에 입력 시 태그 생성
 const addItem = (index) => {
     if (inputs[index].value === "") return;
-    console.log(index);
 
     const li = document.createElement("li");
     const span = document.createElement("span");
-    const removeBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
 
     li.setAttribute("class", "todos__item");
     span.setAttribute("class", "todos__name");
-    removeBtn.setAttribute("class", "todos__delete-btn");
+    deleteBtn.setAttribute("class", "todos__delete-btn");
 
     span.innerText = inputs[index].value;
-    removeBtn.innerText = "X";
+    deleteBtn.innerText = "X";
 
     allItems[index].appendChild(li);
     li.appendChild(span);
-    li.appendChild(removeBtn);
+    li.appendChild(deleteBtn);
 
     inputs[index].value = "";
 };
+
+// deleteBtn 클릭 시 item 삭제 (이벤트 위임!!)
+allItems.forEach((itemList) => {
+    itemList.addEventListener("click", (e) => {
+        if (e.target.tagName !== "BUTTON") return;
+        e.target.closest("li").remove();
+    });
+});
